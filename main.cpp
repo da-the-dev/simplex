9571791c674e1ee4b5ef9b873fc639ae3054d093
da-the-dev <leshiktk@me.com>
2023-10-02 23:38:47 +0300

    Added much needed prints to explain what is happening

--- main.cpp
+++ main.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
-
 #include "matrix.h"
 #include "simplex.h"
 
 using namespace std;
 
 int main() {
+    cout << "Enter the number of variables and the number of constraints:" << endl;
     int variables;  // number of variables
     int constraints;  // number of constraints
     cin >> variables;
     cin >> constraints;
 
+    cout << "Enter the C matrix:" << endl;
     Matrix C(1, variables);
     cin >> C;
 
+    cout << "Enter the A vector (objective function):" << endl;
     Matrix A(constraints, variables);
     cin >> A;
 
+    cout << "Enter the b vector (right-hand side):" << endl;
     Matrix b(constraints, 1);
     cin >> b;
 
+    cout << "Enter the approximation accuracy (as in std::setprecision):" << endl;
     int approximation;
     cin >> approximation;
 
